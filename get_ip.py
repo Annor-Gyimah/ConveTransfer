f86b21b40166dfd2b0272ae16f5f2d8410253e96
Annor-Gyimah <eannor707@gmail.com>
2024-01-16 11:00:24 +0000

    'changes'

--- get_ip.py
+++
@@ -1,9 +0,0 @@
-import socket
-def get_ip(self):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8",80))
-        return s.getsockname()[0]
-    except:
-        return ''
-wifi = get_ip('wlan0')
--- hooks/hook-encodings.py
+++
@@ -1,4 +0,0 @@
-hiddenimports = ['encodings','codecs','io','collections.abc',
-                 'abc','site','os','stat','ntpath','genericpath',
-                 '_sitebuiltins','http','types','enum'
-                ]
